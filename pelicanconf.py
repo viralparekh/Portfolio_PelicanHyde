30750fceb66fcdffec5d12956c89a86f70fe7a8f
Parekh Viral Maheshkumar <parekh.viral@researchweb.iiit.ac.in>
2016-10-19 14:10:08 +0530

    first commit

---
+++ pelicanconf.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+from __future__ import unicode_literals
+
+AUTHOR = u'Viral Parekh'
+SITENAME = u'Viral Parekh'
+SITEURL = 'https://researchweb.iiit.ac.in/~parekh.viral'
+OUTPUT_PATH = '/research/ms/ms2k15/cse/parekh.viral/public_html/'
+STATIC_PATHS = ['images', 'pdfs']
+PATH = 'content'
+
+TIMEZONE = 'Europe/Paris'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+#DEFAULT_PAGINATION = 20
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
+THEME = '/research/ms/ms2k15/cse/parekh.viral/pelican-blueidea'
+NEWEST_FIRST_ARCHIVES = True
+
+
